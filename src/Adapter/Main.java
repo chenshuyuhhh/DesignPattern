bf1a0772919f530ff256703c0b9ecc39116f2c06
chenshuyuhhh <2468493317@qq.com>
2019-01-03 19:12:24 +0800

    修改测试文件，便于别人使用我的代码不用修改文件路径就能运行。

--- src/Adapter/Main.java
+++ src/Adapter/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         FileIO f = new FileProperties();
         try {
-            f.readFromFile("/Users/chenshuyu/Documents/study/程序设计实践/adapterTest.txt");
+            f.readFromFile("AdapterTest/adapterTest.txt");
             f.setValue("year","2019");
             f.setValue("month","1");
             f.setValue("day","1");
-            f.writeFromFile("/Users/chenshuyu/Documents/study/程序设计实践/adapterResult.txt");
+            f.writeFromFile("AdapterTest/adapterResult.txt");
         }catch (IOException e){
             e.printStackTrace();
         }
